13d2b938d32500f8443c3576d930d93e59081c2d
eunicejung3 <69881331+eunicejung3@users.noreply.github.com>
2020-08-18 21:54:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*****************************************************
+*Java Program Name
+******************************************************
+*Eunice Jung
+*8/18/2020
+*CMSC 255-C90
+*****************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Eunice Jung>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1>");
+		System.out.println("<8/18/2020>");
+		System.out.println("<CMSC 255-C90>");
+		System.out.println();
+		}
+}
